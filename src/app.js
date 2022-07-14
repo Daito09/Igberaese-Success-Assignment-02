6b709fdbac927436e6fe28c52c6ba9f55ab07f41
Joshua Igberaese <106039543+Daito09@users.noreply.github.com>
2022-07-14 14:53:13 +0000

    check challenge 1

--- src/app.js
+++ src/app.js
@@ -3,11 +3,20 @@ const assignment = require('./assignment.js');
 const { sumOfNumbers, countEvenNumbers, celsiusToFahrenheit } = assignment;
 
 if (sumOfNumbers && typeof sumOfNumbers === 'function'){
-    console.log(sumOfNumbers([1, 2, 3, 4, -5, 6, 7, 8, 9, 10]));
+    console.log(sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
 
 if (countEvenNumbers && typeof countEvenNumbers === 'function'){
     console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+}
+function countEvenNumbers(arrayOfNumbers){
+  let count = 0
+  for (let i = 0 < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] % 2 == 0){
+      count++
+    }
+  }
+  return count;
 }
 
 if (celsiusToFahrenheit && typeof celsiusToFahrenheit === 'function'){
